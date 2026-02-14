75d1b24b1f1a702ab0843614155ad01b581eaafd
naito-7110 <shige.7110.330@gmail.com>
2026-02-14 17:57:26 +0900

    chore(wezterm): change opacity 0.50 -> 0.70

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -4,7 +4,7 @@ local config = wezterm.config_builder()
 config.automatically_reload_config = true
 config.font_size = 12.0
 config.use_ime = true
-config.window_background_opacity = 0.50
+config.window_background_opacity = 0.70
 
 -- keybind
 config.keys = {
