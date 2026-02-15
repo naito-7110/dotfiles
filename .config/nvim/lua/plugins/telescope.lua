ca921886d93d407507e408ffed6a34a2f2cb2e95
naito-7110 <shige.7110.330@gmail.com>
2026-02-15 10:06:47 +0900

    refactor(nvim): remove blank

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,6 +1,6 @@
 ---@type LazyPluginSpec
 return {
-    'nvim-telescope/telescope.nvim', 
+    'nvim-telescope/telescope.nvim',
     branch = '0.1.x',
     dependencies = { 'nvim-lua/plenary.nvim' },
     event = 'VeryLazy',
