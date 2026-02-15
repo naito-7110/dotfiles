fad3c9ff15f09d40dd32cee421f2634e06502dbe
naito-7110 <shige.7110.330@gmail.com>
2026-02-15 19:41:42 +0900

    feat(wezterm): change opacity settings

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -6,7 +6,6 @@ config.automatically_reload_config = true
 config.font_size = 12.0
 config.line_height = 1.0
 config.use_ime = true
-config.window_background_opacity = 0.70
 
 -- 非アクティブなペインを暗くする
 config.inactive_pane_hsb = {
@@ -14,6 +13,29 @@ config.inactive_pane_hsb = {
   brightness = 0.7,
 }
 
+-- 透過設定
+local default_opacity = 0.70
+local min_opacity = 0.5
+local max_opacity = 1.0
+local opacity = default_opacity
+config.window_background_opacity = default_opacity
+config.macos_window_background_blur = 0
+
+function adjust_opacity(window, delta)
+  opacity = math.max(max_opacity, math.min(min_opacity, opacity + delta))
+
+  window:set_config_overrides({
+    window_background_opacity = opacity,
+  })
+end
+
+wezterm.on("increase-opacity", function(window, _)
+  adjust_opacity(window, 0.05)
+end)
+
+wezterm.on("decrease-opacity", function(window, _)
+  adjust_opacity(window, -0.05)
+end)
 
 -- keybind
 config.keys = {
@@ -35,6 +57,10 @@ config.keys = {
 
   -- ペイン最大化（トグルズーム）
   { key = 'z', mods = 'CMD', action = act.TogglePaneZoomState },
+
+  -- opacity
+--  { key = 'UpArrow', mods = "CMD|SHIFT", action = act.EmitEvent("increase-opacity")},
+--  { key = 'DownArrow', mods = "CMD|SHIFT", action = act.EmitEvent("decrease-opacity")},
 }
 
 return config
