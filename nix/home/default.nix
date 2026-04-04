464fc06161e3dd7e3ab08588bdd37c9f32180b7d
naito-7110 <shige.7110.330@gmail.com>
2026-04-04 21:51:20 +0900

    fix(nix): add convertor into home nix

--- nix/home/default.nix
+++ nix/home/default.nix
@@ -4,7 +4,7 @@
     ./git.nix
     ./zsh.nix
   ];
-  
+
   programs = {
     starship.enable = true;
     neovim.enable = true;
@@ -19,13 +19,13 @@
     username = "n7110";
     homeDirectory = "/Users/n7110";
     stateVersion = "24.05";
-    
+
     file = {
-      ".config/starship.toml".source =  ../../.config/starship/starship.toml;
+      ".config/starship.toml".source = ../../.config/starship/starship.toml;
       ".config/nvim".source = ../../.config/nvim;
       ".config/wezterm".source = ../../.config/wezterm;
     };
-    
+
     packages = with pkgs; [
       direnv
       nix-direnv
@@ -36,9 +36,13 @@
       kubectl
       gh
       git-secrets
-      
+
       vscode
       claude-code
+
+      ffmpeg
+      texliveFull
+      poppler-utils
     ];
   };
 }
