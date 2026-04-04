9f6f379ff277ef249e5799dabba533e1a3439121
naito-7110 <shige.7110.330@gmail.com>
2026-04-04 22:39:28 +0900

    fix(nix): edit node25 flake template

--- nix/templates/node25/flake.nix
+++ nix/templates/node25/flake.nix
@@ -3,21 +3,24 @@
 
   inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
 
-  outputs = { self, nixpkgs }:
-  let
-    system = "aarch64-darwin";
-    pkgs = import nixpkgs { inherit system; };
-  in {
-    devShells.${system}.default = pkgs.mkShell {
-      packages = with pkgs; [
-        nodejs_25
-        pnpm
-        vscode-langservers-extracted
-      ];
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "aarch64-darwin";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          nodejs_25
+          pnpm
+          vscode-langservers-extracted
+          typescript-language-server
+        ];
 
-      shellHook = ''
-        echo "Node: $(node -v)"
-      '';
+        shellHook = ''
+          echo "Node: $(node -v)"
+        '';
+      };
     };
-  };
 }
